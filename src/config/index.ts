ff0d4cb36cbacea46b460de6f48ba74b2bf6d9d1
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-03-14 16:44:48 +0000

    Cyclic Host

--- src/config/index.ts
+++ src/config/index.ts
@@ -20,7 +20,7 @@ export interface IConfig {
 }
 
 const defaultConfig: IConfig = {
-  host: "https://sore-cyan-bull-tutu.cyclic.app",
+  host: "::",
   port: 9000,
   expire_timeout: 5000,
   alive_timeout: 60000,
